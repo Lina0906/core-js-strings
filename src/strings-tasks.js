9287c34c65607e7939f4d71dfc9e5749cfa1e49f
Lina Churbanova <lina.moiseeva@gmail.com>
2023-12-21 19:15:47 +0400

    feat: add 27 tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -427,8 +427,18 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    let char = str.charAt(i);
+    if (char === char.toUpperCase()) {
+      char = char.toLowerCase();
+    } else {
+      char = char.toUpperCase();
+    }
+    result += char;
+  }
+  return result;
 }
 
 /**
